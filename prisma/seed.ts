5886332c25cff7befb499f92e20d573a4d948cfc
Darialyphia <lo.pennequin@gmail.com>
2026-01-12 23:31:25 +0100

    update seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaPg } from '@prisma/adapter-pg'
+import 'dotenv/config'
+import { PrismaClient } from '../src/server/db/generated/prisma/client/client'
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const prisma = new PrismaClient({
+  adapter,
+})
 
 async function main() {
   const anonymous = await prisma.user.upsert({
